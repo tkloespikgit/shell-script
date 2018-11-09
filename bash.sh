33babaf3bdbe8d1c1754fee41969fc4bca335813
hongpeng.chen <tkloespik@gmail.com>
2018-11-09 14:59:39 +0800

    init

---
+++ bash.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+checkCommandExist(){
+    command -v $1 >/dev/null 2>&1 || { echo "I require $1 but it's not installed.  Aborting." >&2; exit 1; }
+}
+
+readBoolStr(){
+    if test $# == 1  ; then
+        echo $1 yes/no
+        read ANS
+        readBoolStr "$1" $ANS
+        ret=$?
+        return $ret
+    elif test $# == 2  ; then
+        case $2 in
+        yes|YES|Yes)
+            return 1
+        ;;
+        no|NO|No)
+            return 0
+        ;;
+        *)
+            readBoolStr "$1"
+            ret=$?
+            return $ret
+        ;;
+        esac
+    else
+        readBoolStr "$1"
+        ret=$?
+        return $ret
+    fi
+}
+
+function contains() {
+    local n=$#
+    local value=${!n}
+    for ((i=1;i < $#;i++)) {
+        if [ "${!i}" == "${value}" ]; then
+            echo "y"
+            return 0
+        fi
+    }
+    echo "n"
+    return 1
+}
+
+function operationSelect(){
+    paramArray=()
+
+    echo "We support follow select："
+    echo
+    while [ $# -gt 0 ]
+    do
+        paramArray+=("$1")
+        echo $1
+        shift
+    done
+
+    echo
+    echo
+    echo "Please select your env:"
+
+    read ANS
+
+    if [ $(contains "${paramArray[@]}" ${ANS}) == "y" ]; then
+        __=$ANS
+    else
+        operationSelect "${paramArray[@]}"
+    fi
+}
+
+
+function testOperationSelect(){
+    paramArray=(a "bb" "cc  dd" "ee   ff   gg")
+    operationSelect "${paramArray[@]}"
+
+    echo your selection is: ${__}
+}
---
+++ web.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+
+composerDownload(){
+    if [ -f "composer.json" ]
+    then
+        echo Start composer download
+        if [ -f "composer.phar" ]
+        then
+            echo "composer.phar has been exist."
+        else
+            curl -sS https://getcomposer.org/installer | php
+            echo Composer download success
+        fi
+    else
+        echo The project does not contain composer.json, so you dont need download composer.phar
+    fi
+}
+
+composerInstall(){
+    if [ -f "composer.json" ]
+    then
+        #    rm -r -f vendor
+        #    rm -r -f composer.lock
+        php composer.phar install
+    else
+        echo The project does not contain composer.json, so you dont need do composer.phar install
+    fi
+}
+
+composerUpdate(){
+    if [ -f "composer.json" ]
+    then
+        #    rm -r -f vendor
+        #    rm -r -f composer.lock
+        php composer.phar update
+    else
+        echo The project does not contain composer.json, so you dont need do composer.phar install
+    fi
+}
+
+
+calcBranch(){
+	case $1 in
+	stage)
+        	echo stage
+	;;
+	production)
+        	echo production
+	;;
+	*)
+        	echo stage
+	;;
+	esac
+}
+
+pull_web_source(){
+    echo
+    echo do git pull $1
+    echo
+    git reset --hard
+    git pull
+
+    BRANCH=`calcBranch $1`
+
+    exists=`git show-ref refs/heads/$BRANCH`
+    if [ -n "$exists" ]; then
+        echo $BRANCH branch exists!
+    else
+        git checkout -b $BRANCH origin/$BRANCH
+    fi
+    git checkout $BRANCH
+    git pull
+    echo
+    echo git pull web success
+    echo
+}
